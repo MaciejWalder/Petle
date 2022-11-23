94f7be4aadf0c7727afeaf30ed56615f95e095b4
Maciej <walder_m@wp.pl>
2022-11-23 20:55:59 +0100

    Calkulator build for switch ver 2

--- src/Kalkulator_switch1.java
+++ src/Kalkulator_switch1.java
@@ -4,10 +4,10 @@ public class Kalkulator_switch1 {
     public static void main(String[] args) {
         System.out.println("Witaj w kalkulatorze \nWybierz pierwszą liczbe");
         Scanner scanner=new Scanner(System.in);
-        int number1=scanner.nextInt();
+        double number1=scanner.nextDouble();
         System.out.println("Wybierz drugą liczbę");
-        int number2=scanner.nextInt();
-        System.out.println("Wybierz rodzaj równania: \n1. Dodawanie \n2. Odejmowanie \n3. Mnożenie \n4. Dzielenie");
+        double number2=scanner.nextDouble();
+        System.out.println("Wybierz równanie: \n1. Dodawanie \n2. Odejmowanie \n3. Mnożenie \n4. Dzielenie");
         int inputNumber = scanner.nextInt();
         switch (inputNumber){
             case 1:
